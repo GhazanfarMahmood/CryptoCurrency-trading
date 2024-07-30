ca8f2a585ddfb5e4b325b80abee66a40f734b63f
Ghazanfar Mahmood <ghazanfarmahmood14@gmail.com>
2024-07-30 21:39:26 +0500

    Fetching Data from Api and displaying

--- src/context/CoinContext.jsx
+++ src/context/CoinContext.jsx
@@ -1,11 +1,35 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const CoinContext = createContext();
 
 const CoinContextProvider = (props) =>{
 
-    const contextValue = {
+    const [allCoin, setAllCoin] = useState([]);
+    const [currency, setCurrency] = useState({
+        name : "usd",
+        symbol: "$",
+    });
 
+    const fetchAllCoin = async () =>{
+        const options = {
+            method: 'GET',
+            headers: {accept: 'application/json', 'x-cg-demo-api-key': 'CG-7zyr7wa8erCwsxvrJvr56sLX'}
+          };
+          
+          fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency.name}`, options)
+            .then(response => response.json())
+            .then(response => setAllCoin(response))
+            .catch(err => console.error(err));
+    }
+
+   useEffect(() =>{
+    fetchAllCoin();
+      }, [currency]) // instead of calling the api first time we want to call the api every time when we change the currency so we pass currency in fetch
+
+
+
+    const contextValue = {
+        allCoin, currency, setCurrency
     }
 
     return (
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,29 +1,60 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from "react";
 
-import './Home.css'
+import "./Home.css";
+import { CoinContext } from "../../context/CoinContext";
 
 const Home = () => {
+  const { allCoin, currency } = useContext(CoinContext);
+  const [displayCoin, setDisplayCoin] = useState([]);
+
+  useEffect(() => {
+    setDisplayCoin(allCoin);
+  }, [allCoin]);
+
   return (
-    <div className='home'>
-      <div className='hero'>
-        <h1>Largest <br /> Crypto Marketplace</h1>
-        <p>Welcome to the world's largest market cryptocurrency marketplace. Sign up to explore more about cryptos.</p>
+    <div className="home">
+      <div className="hero">
+        <h1>
+          Largest <br /> Crypto Marketplace
+        </h1>
+        <p>
+          Welcome to the world's largest market cryptocurrency marketplace. Sign
+          up to explore more about cryptos.
+        </p>
         <form>
-          <input type="text" placeholder='Search crypto....' />
-          <button type='submit'>Search</button>
+          <input type="text" placeholder="Search crypto...." />
+          <button type="submit">Search</button>
         </form>
       </div>
-      <div className='crypto-table'>
-        <div className='table-layout'>
+      <div className="crypto-table">
+        <div className="table-layout">
           <p>#</p>
           <p>Coins</p>
           <p>Price</p>
-          <p style={{textAlign: 'center'}}>24H Change</p>
-          <p className='market-cap'>Market Cap</p>
+          <p style={{ textAlign: "center" }}>24H Change</p>
+          <p className="market-cap">Market Cap</p>
         </div>
+        {displayCoin.slice(0, 10).map((item, index) => (
+          <div className="table-layout" key={index}>
+            <p>{item.market_cap_rank}</p>
+            <div>
+              <img src={item.image} alt="image" />
+              <p>{item.name + " - " + item.symbol}</p>
+            </div>
+            <p>
+              {currency.symbol} {item.current_price.toLocaleString()}
+            </p>
+            <p style={item.price_change_percentage_24h < 0 ? {color : "#ff4646", textAlign : "center"} : {color : "#00D515", textAlign : "center"}}>
+              {Math.floor(item.price_change_percentage_24h * 100) / 100 }
+            </p>
+            <p className="market-cap">
+              {currency.symbol} {item.market_cap.toLocaleString()}
+            </p>
+          </div>
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
